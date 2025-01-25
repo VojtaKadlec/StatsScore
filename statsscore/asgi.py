71a62667c49f078faece365224979b34cf9acf14
unknown <vojtech.kadlec@student.gyarab.cz>
2025-01-25 17:50:37 +0100

    initial commit

---
+++ statsscore/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for statsscore project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'statsscore.settings')
+
+application = get_asgi_application()
