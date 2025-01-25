9d39ba5ab3b4b1c95d8d4431cc4691b01f6dd1a8
unknown <vojtech.kadlec@student.gyarab.cz>
2025-01-25 20:06:36 +0100

    idk

--- teamy/models.py
+++ teamy/models.py
@@ -1,13 +1,15 @@
-from django.db import models 
+from django.db import models
 from sporty.models import Sport
 
 class Team(models.Model):
-    jmeno=models.CharField(max_length=100)
-    sport=models.ForeignKey(Sport,on_delete=models.CASCADE)
-    hraci=models.ManyToManyField("Hrac")
-    
+    jmeno = models.CharField(max_length=100)
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
+    hraci = models.ManyToManyField("Hrac", related_name="teams")  
+
 class Hrac(models.Model):
-    jmeno=models.CharField(max_length=100)
-    team=models.ForeignKey(Team, on_delete=models.CASCADE)
-    cislo=models.IntegerField()
+    jmeno = models.CharField(max_length=100)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="players")  
+    cislo = models.IntegerField()
+
+
 
--- zapasy/models.py
+++ zapasy/models.py
@@ -3,8 +3,15 @@ from sporty.models import Sport
 from teamy.models import Team
 
 class Zapas(models.Model):
-    sport=models.ForeignKey(Sport, on_delete=models.CASCADE)
-    domaci=models.ForeignKey(Team, on_delete=models.CASCADE)
-    hoste=models.ForeignKey(Team, on_delete=models.CASCADE)
-    datum=models.DateField()
-    status=models.CharField(choices=[("nadchazejici", "Nadcházející"), ("probihajici", "Probíhající"), ("ukoncene", "Ukončené")], max_length=20)
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
+    domaci = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='domaci_zapasy')
+    hoste = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='hoste_zapasy')
+    datum = models.DateField()
+    status = models.CharField(
+        choices=[
+            ("nadchazejici", "Nadcházející"),
+            ("probihajici", "Probíhající"),
+            ("ukoncene", "Ukončené")
+        ],
+        max_length=20
+    )
